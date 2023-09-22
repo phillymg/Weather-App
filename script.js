f3fa93fffa408e3e51ba235a8c4c24ca465366c7
Philmg <phillipmguetterman@att.net>
2023-09-22 18:06:27 -0500

    create event listener for form

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ var APIKey = "72c3cee628c2f88baf91042dc00b51a8";
 var city;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-fetch(queryURL)
+// fetch(queryURL)
+
+const formquery = document.querySelector("#searchform");
+
+formquery.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    console.log(data.get("search"));
+})
+
+
+
